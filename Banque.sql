a82e0a8715fb179f01af46eb3828a28a4d78bdea
Ramjoh <romjah@orange.fr>
2023-01-17 12:17:04 +0100

    sql

---
+++ Banque.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mar. 17 jan. 2023 à 11:15
+-- Version du serveur :  5.7.34
+-- Version de PHP : 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `Banque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bankaccounts`
+--
+
+CREATE TABLE `bankaccounts` (
+  `id_user` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `monnaie` varchar(255) NOT NULL,
+  `created _at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `currencies`
+--
+
+CREATE TABLE `currencies` (
+  `id_monnaie` int(11) NOT NULL,
+  `nom_monnaie` int(11) NOT NULL,
+  `taux_change` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `deposits`
+--
+
+CREATE TABLE `deposits` (
+  `id_user` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  `date_depos` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `monnaie` int(11) NOT NULL,
+  `description_depot` text NOT NULL,
+  `id_depo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id_user` int(11) NOT NULL,
+  `montant_depot` int(100) NOT NULL,
+  `date_depos` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_retrait` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description_depot` text NOT NULL,
+  `montant_retrait` int(100) NOT NULL,
+  `id_transaction` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `role` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created _at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `grade` int(4) NOT NULL,
+  `id_transaction` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `withdrawals`
+--
+
+CREATE TABLE `withdrawals` (
+  `id_user` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  `monnaie` varchar(200) NOT NULL,
+  `date_retrait` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `montant_retrait` int(100) NOT NULL,
+  `id_retrait` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `bankaccounts`
+--
+ALTER TABLE `bankaccounts`
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `bankaccounts`
+--
+ALTER TABLE `bankaccounts`
+  ADD CONSTRAINT `bankaccounts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
