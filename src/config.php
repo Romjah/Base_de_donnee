e6fb5d835dab3565b1895f0985f5b4196ed72c45
leo <leoyng95@gmail.com>
2023-01-18 11:24:38 +0100

    committed

--- src/config.php
+++ src/config.php
@@ -4,8 +4,6 @@ $config = ['db' => [
     'host' => 'localhost',
     'name' => 'banque',
     'port' => 3306,
-    'name' => 'Banque',
-    'port' => 8888,
     'user' => 'root',
     'pass' => 'root'
 ]];
