076ed324fb8740d49c1561ec18de0f15db3c5d0e
busquet jordan <jordan@mbp-de-admin.edu.sir>
2023-01-18 15:11:19 +0100

    page log

---
+++ www/connextion.php
@@ -0,0 +1,21 @@
+<?php
+require_once DIR . '/../src/init.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="stylec.css">
+    <title>Connexion</title>
+</head>
+<body>
+ <form method="POST" action="">
+        <input type="text" name="email" placeholder="Email" >
+        <br>
+        <input type="password" name="mdp" placeholder="mot de passe">
+        <br>
+        <input type="submit" value="connexion" name="connexion">
+ </form>
+</body>
+</html>
---
+++ www/page_tunas.php
@@ -0,0 +1,17 @@
+<?php
+require_once DIR . '/../src/init.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="stylec.css">
+    <title>Page perso</title>
+</head>
+<body>
+ <form method="POST" action="">
+        <input type="submit" value="redirection vers le dépôt" name="redirection vers le dépôt">
+ </form>
+</body>
+</html>
