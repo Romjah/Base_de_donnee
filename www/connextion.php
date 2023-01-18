b0ec4aa9ae484be158b38fa5e28291c63bb1b243
busquet jordan <jordan@macbook-pro-de-admin.home>
2023-01-18 23:42:03 +0100

    connextion

--- www/connextion.php
+++ www/connextion.php
@@ -1,43 +1,87 @@
-<?php
-require_once __DIR__ . '/../src/init.php';
-if(isset($_POST['connexion'])){
-    if(!empty($_POST['email']) && !empty($_POST['mdp'])){
-        $email = $_POST["email"];
-        $mdp = hash('sha256', $_POST["mdp"]);
-    
-        $reccupUser = $bdd->prepare('SELECT * FROM user WHERE email= ? and mdp= ?');
-        $reccupUser->execute(array($email, $mdp));
-
-        if($reccupUser->rowCount() > 0){
-            $_SESSION['email'] =$email;
-            $_SESSION['mdp'] = $mdp;
-            $_SESSION['id_user'] = $reccupUser->fetch()['id_user'];
-            header("location:index.php");
-
-        } else{
-            echo "votre email ou mot de passe est incorrect";
-        }
-
-    }else{
-        echo "veuillez completer les champs";
-    }
-    }
-?>
-
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="stylec.css">
-    <title>Connexion</title>
-</head>
-<body>
- <form method="POST" action="">
-        <input type="text" name="email" autocomplete="off" placeholder="Email" >
-        <br>
-        <input type="password" name="mdp" autocomplete="off" placeholder="mot de passe">
-        <br>
-        <input type="submit" value="connexion" name="connexion">
- </form>
-</body>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="NoS1gnal"/>
+
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <title>Connexion</title>
+        </head>
+        <body>
+        
+        <div class="login-form">
+             <?php 
+                if(isset($_GET['login_err']))
+                {
+                    $err = htmlspecialchars($_GET['login_err']);
+
+                    switch($err)
+                    {
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte non existant
+                            </div>
+                        <?php
+                        break;
+                    }
+                }
+                ?> 
+            
+            <form action="connexion.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                </div>   
+            </form>
+            <p class="text-center"><a href="inscription.php">Inscription</a></p>
+        </div>
+        <style>
+            .login-form {
+                width: 340px;
+                margin: 50px auto;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+            }
+        </style>
+        </body>
 </html>
