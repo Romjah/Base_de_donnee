35cff782c149475907940d1949c84a024bbce58c
Ramjoh <romjah@orange.fr>
2023-01-18 21:22:18 +0100

    rename de index en connexion

--- www/connexion.php
+++ www/connexion.php
@@ -1,37 +1,87 @@
-<?php 
-    session_start(); // Démarrage de la session
-    require_once __DIR__ . '../../src/config.php'; // On inclu la connexion à la bdd
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="NoS1gnal"/>
 
-    if(!empty($_POST['email']) && !empty($_POST['password'])) // Si il existe les champs email, password et qu'il sont pas vident
-    {
-        // Patch XSS
-        $email = htmlspecialchars($_POST['email']); 
-        $password = htmlspecialchars($_POST['password']);
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <title>Connexion</title>
+        </head>
+        <body>
         
-        $email = strtolower($email); // email transformé en minuscule
-        
-        // On regarde si l'utilisateur est inscrit dans la table users
-        $check = $bdd->prepare('SELECT fullname, email, password, token FROM users WHERE email = ?');
-        $check->execute(array($email));
-        $data = $check->fetch();
-        $row = $check->rowCount();
-        
-        
-
-        // Si > à 0 alors l'utilisateur existe
-        if($row > 0)
-        {
-            // Si le mail est bon niveau format
-            if(filter_var($email, FILTER_VALIDATE_EMAIL))
-            {
-                // Si le mot de passe est le bon
-                if(password_verify($password, $data['password']))
+        <div class="login-form">
+             <?php 
+                if(isset($_GET['login_err']))
                 {
-                    // On créer la session et on redirige sur landing.php
-                    $_SESSION['users'] = $data['token'];
-                    header('Location: landing.php');
-                    die();
-                }else{ header('Location: index.php?login_err=password'); die(); }
-            }else{ header('Location: index.php?login_err=email'); die(); }
-        }else{ header('Location: index.php?login_err=already'); die(); }
-    }else{ header('Location: index.php'); die();} // si le formulaire est envoyé sans aucune données
+                    $err = htmlspecialchars($_GET['login_err']);
+
+                    switch($err)
+                    {
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte non existant
+                            </div>
+                        <?php
+                        break;
+                    }
+                }
+                ?> 
+            
+            <form action="connexion.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                </div>   
+            </form>
+            <p class="text-center"><a href="inscription.php">Inscription</a></p>
+        </div>
+        <style>
+            .login-form {
+                width: 340px;
+                margin: 50px auto;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+            }
+        </style>
+        </body>
+</html>
