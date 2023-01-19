20edd827b48aa3475bebab39e86b70c94861bd80
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-01-19 21:42:09 +0100

    correction probleme inscription

--- www/inscription.php
+++ www/inscription.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../src/init.php';
 
 if (isset($_POST["inscription"])){
     if(!empty($_POST['email']) || !empty($_POST['mdp'])){
+        $nom= htmlspecialchars($_POST["nom"]);
+        $prenom= htmlspecialchars($_POST["prenom"]);
         $email = $_POST["email"];
         $mdp = hash('sha256', $_POST["mdp"]);
         $token= bin2hex(openssl_random_pseudo_bytes(64));
